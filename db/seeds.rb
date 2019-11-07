1157cfade595c19128dcc88a41db64333a403cd5
halkity <saito75061@gmail.com>
2019-11-07 16:43:09 +0900

    create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |n|
+  Event.create(title: "イベント#{n+1}",start_datetime: "2019-11-#{n+1} #{n+6}:00:00",end_datetime: "2019-11-#{n+1} #{n+9}:00:00")
+end
